921ef3f46ce1c18d904a5d01a4f0f08705e9c0c8
Sean Philippi <sean.philippi@gmail.com>
2019-09-26 10:17:29 -0500

    initial commit

---
+++ src/App/index .js
@@ -0,0 +1,17 @@
+import React from './node_modules/react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Crypto-Tracker
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
