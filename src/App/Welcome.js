75ed17c6e46fd740f5ca9d06d0d68f8415138e87
Sean Philippi <sean.philippi@gmail.com>
2019-11-26 16:59:09 -0600

    styling, styled-components experiments

--- src/App/Welcome.js
+++ src/App/Welcome.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Button = styled.div`
+const Button = styled.button`
   color: red;
-`
+  border: solid black 1px;
+  width: 5rem;
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`;
+
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 const Welcome = () => (
-  <h1>
-    Welcome to Crypto-Tracker
-  </h1>
+<>
+    <h1>
+      Welcome to Crypto-Tracker
+    </h1>
+    <Button>Yo</Button>
+    <Button primary>Yo dawg</Button>
+    <TomatoButton>Tomato</TomatoButton>
+</>
 );
 
 export default Welcome;
