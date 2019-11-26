337347a3b13897efaa99b17e0d985fe7aa2e38ea
Sean Philippi <sean.philippi@gmail.com>
2019-11-26 16:44:11 -0600

    added styled-components and babel/runtime

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Welcome from './Welcome';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/App/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.div`
+  color: red;
+`
+
+const Welcome = () => (
+  <h1>
+    Welcome to Crypto-Tracker
+  </h1>
+);
+
+export default Welcome;
