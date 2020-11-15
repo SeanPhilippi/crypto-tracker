045dae288f15f82e628496535cf156e232e37e2a
SeanPhilippi <sean.philippi@gmail.com>
2020-11-14 20:04:25 -0600

    moved App out of components

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from './node_modules/react';
 import ReactDOM from './node_modules/react-dom';
-import App from './index ';
+import App from '../App.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
