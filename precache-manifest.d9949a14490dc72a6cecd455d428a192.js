ff3d18f48ba536864804a5ce70bf060d681b5b25
SeanPhilippi <sean.philippi@gmail.com>
2021-03-13 15:21:15 -0600

    Updates

---
+++ precache-manifest.d9949a14490dc72a6cecd455d428a192.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168fde85cfdfd88dd0bf7d88d0e4cfc2",
+    "url": "/crypto-tracker/index.html"
+  },
+  {
+    "revision": "f635c5473f8fdcc176b5",
+    "url": "/crypto-tracker/static/css/main.6a993d48.chunk.css"
+  },
+  {
+    "revision": "ddc18858cc168329fdeb",
+    "url": "/crypto-tracker/static/js/2.3cd82475.chunk.js"
+  },
+  {
+    "revision": "f635c5473f8fdcc176b5",
+    "url": "/crypto-tracker/static/js/main.e9d43b7c.chunk.js"
+  },
+  {
+    "revision": "749d2d0ce861667354a4",
+    "url": "/crypto-tracker/static/js/runtime-main.249f0cdb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7993ec56665ef6051522b3fd4282b994.js"
+  "/crypto-tracker/precache-manifest.d9949a14490dc72a6cecd455d428a192.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crypto-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
